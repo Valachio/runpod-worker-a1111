362ae78d72fd196602e95a6acd3540a1d812a9b7
Jack Zhang <valachio123@gmail.com>
2024-01-13 18:35:44 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,77 +28,13 @@ echo "Installing A1111 Web UI"
 wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/install-automatic.py
 python3 -m install-automatic --skip-torch-cuda-test
 
-echo "Cloning ControlNet extension repo"
-cd /workspace/stable-diffusion-webui
-git clone --depth=1 https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
-
-echo "Cloning the ReActor extension repo"
-git clone --depth=1 https://github.com/Gourieff/sd-webui-reactor.git extensions/sd-webui-reactor
-
-echo "Cloning the After Detailer extension repo"
-git clone --depth=1 https://github.com/Bing-su/adetailer.git extensions/adetailer
-
-echo "Installing dependencies for ControlNet"
-cd /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
-pip3 install -r requirements.txt
-
-echo "Installing dependencies for ReActor"
-cd /workspace/stable-diffusion-webui/extensions/sd-webui-reactor
-pip3 install -r requirements.txt
-pip3 install onnxruntime-gpu
-
-echo "Installing dependencies for After Detailer"
-cd /workspace/stable-diffusion-webui/extensions/adetailer
-python3 -m install
-
-echo "Installing the model for ReActor"
-mkdir -p /workspace/stable-diffusion-webui/models/insightface
-cd /workspace/stable-diffusion-webui/models/insightface
-wget https://github.com/facefusion/facefusion-assets/releases/download/models/inswapper_128.onnx
-
-echo "Configuring ReActor to use the GPU instead of CPU"
-echo "CUDA" > /workspace/stable-diffusion-webui/extensions/sd-webui-reactor/last_device.txt
-
 echo "Installing RunPod Serverless dependencies"
 cd /workspace/stable-diffusion-webui
 pip3 install huggingface_hub runpod
 
 echo "Downloading Deliberate v2 model"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget -O deliberate_v2.safetensors https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors
-
-echo "Downloading SDXL base model"
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-
-echo "Downloading SDXL Refiner"
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
-
-echo "Downloading SD 1.5 VAE"
-cd /workspace/stable-diffusion-webui/models/VAE
-wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
-
-echo "Downloading SDXL VAE"
-wget https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors
-
-echo "Downloading SD 1.5 ControlNet models"
-mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
-cd /workspace/stable-diffusion-webui/models/ControlNet
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
-wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
-
-echo "Downloading SDXL ControlNet models"
-wget https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors
-
-echo "Downloading Upscalers"
-mkdir -p /workspace/stable-diffusion-webui/models/ESRGAN
-cd /workspace/stable-diffusion-webui/models/ESRGAN
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
+wget -O unicorn.safetensors https://civitai.com/api/download/models/223670
 
 echo "Creating log directory"
 mkdir -p /workspace/logs
